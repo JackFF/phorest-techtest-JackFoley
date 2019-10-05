119dc2318d0300eeaf8012f942818b23ee099b48
Jack Foley <j.foley11@nuigalway.ie>
2019-10-05 19:57:27 +0100

    Added the add voucher functionality

--- routes/clients.js
+++ routes/clients.js
@@ -8,7 +8,7 @@ router.get('/clientSearch', (req, res) => {
     res.render('clients/clientSearch');
 });
 
-//  client search results route
+//  Client search results route
 router.get('/clientSearchResults', (req, res) => {
 
     //  Getting email and phone querys
@@ -20,7 +20,9 @@ router.get('/clientSearchResults', (req, res) => {
     const password = 'VMlRo/eh+Xd8M~l';
     const options = {
 
-    url: `http://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client?email=${email}&phone=${phone}`,
+    //?email=${email}&phone=${phone}
+
+    url: `http://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/client`,
     auth: {
 
         user: username,
@@ -47,6 +49,55 @@ router.get('/clientSearchResults', (req, res) => {
     });
 });
 
+//  Add voucher route
+router.get('/addVoucher', (req, res) => {
+
+    const clientId = req.query.clientId;
+    const amount = req.query.amount;
+
+    console.log('Amount: ' + amount);
+
+    //  Authentication settings
+    const username = 'global/cloud@apiexamples.com';
+    const password = 'VMlRo/eh+Xd8M~l';
+
+    request.post('https://api-gateway-dev.phorest.com/third-party-api-server/api/business/eTC3QY5W3p_HmGHezKfxJw/voucher', {
+
+        auth: {
+
+            user: username,
+            password: password
+        },
+
+        json: {
+
+            clientId: clientId,
+            creatingBranchId: "SE-J0emUgQnya14mOGdQSw",
+            expiryDate: "2019-10-05T16:05:58.806Z",
+            issueDate: "2019-10-05T16:05:58.806Z",
+            originalBalance: 133.15
+        }
+    }, (err, res, body) => {
+
+        if (err) {
+
+            console.log(err);
+        }
+
+        else {
+
+            console.log('Res1: ' + res);
+            console.log('Body1: ' + body);
+
+            console.log('Res2: ' + JSON.stringify(res));
+            console.log('Body2: ' + JSON.stringify(body));
+        }
+    });
+
+    res.render('clients/clientSearch');
+});
+
+//  Client search POST
 router.post('/clientSearch', (req, res) => {
 
     const email = req.body.email;
@@ -54,6 +105,15 @@ router.post('/clientSearch', (req, res) => {
 
     //  Redirect to clients result page and pass email and phone inputs
     res.redirect(`/clients/clientSearchResults?email=${email}&phone=${phone}`);
+});
+
+//  Client search POST to add voucher
+router.post('/clientSearchResults', (req, res) => {
+
+    const clientId = req.body.clientId;
+    const amount = req.body.amount;
+
+    res.redirect(`/clients/addVoucher?clientId=${clientId}&amount=${amount}`);
 });
 
 module.exports = router;
